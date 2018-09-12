f94a715ef7053a29942c7de6be516b09ab27fa6b
YouZhengHua <aaazzzz168@gmail.com>
2018-09-13 00:25:57 +0800

    Week2 first commit.

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,5 +1,13 @@
 function add(a, b) {
-  
+	var rtnStr = "";
+	var carryNum = 0;
+	for(var i = a.length - 1; i >= 0; i--){
+		var sum = Number(a[i]) + Number(b[i]) + carryNum;
+		carryNum = sum >= 10 ? 1 : 0;
+		sum = sum >= 10 ? sum - 10 : sum;
+		rtnStr = sum.toString() + rtnStr;
+	}
+	return rtnStr;
 }
 
 module.exports = add;
