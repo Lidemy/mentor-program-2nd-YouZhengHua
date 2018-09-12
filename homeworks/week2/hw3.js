88e2acc009e08407f4f1d8f5cc2e38501d8322cd
YouZhengHua <aaazzzz168@gmail.com>
2018-09-13 00:25:57 +0800

    Week2 first commit.

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function isPrime(n) {
-  
+	var rtnBool = n != 1;
+	for(var i = 2; i < n; i++){
+		rtnBool = n % i == 0;
+		if(!rtnBool){
+			break;
+		}
+	}
+	return rtnBool;
 }
 
 module.exports = isPrime
